26437ff01ab6d1bd7536a28dc5a6611d2c25581d
keerthana <surelakshmihk@gmail.com>
2025-09-24 15:54:17 +0530

    C:\Users\KEERTHANA.S\Desktop\web development\FruitsList.java

---
+++ FruitsList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class FruitsList {
+    public static void main(String[] args) {
+        // Create an ArrayList of Strings
+        ArrayList<String> fruits = new ArrayList<>();
+
+        // Add fruits to the list
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Orange");
+        fruits.add("Grapes");
+
+         System.out.println("Fruits: " + fruits);
+    }
+}
+
+
